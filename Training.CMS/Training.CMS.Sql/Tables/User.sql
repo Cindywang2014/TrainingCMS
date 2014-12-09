3d67bb5e619c12eedb20f25c8af2d735abef9e5e
Cindywang2014 <Cindy.wang@Delawareconsulting.com>
2014-12-09 11:19:54 +0800

    Revert "First update files"
    This reverts commit 5b8b39fc2cdd20c3ef2419defaec0e78ca8c8cbf.

--- Training.CMS/Training.CMS.Sql/Tables/User.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[User]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,  
-    [UserName] NVARCHAR(50) NULL, 
-    [Password] NVARCHAR(50) NULL, 
-    [EmailAddress] NVARCHAR(100) NULL, 
-    [Country] NVARCHAR(50) NULL, 
-    [Region] NVARCHAR(50) NULL
-)
