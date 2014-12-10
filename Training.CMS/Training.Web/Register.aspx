bd2ac606b989ed6040063e345ff4337ffba010dd
mitangtang <1048329163@qq.com>
2014-12-10 16:43:04 +0800

    Register

---
+++ Training.CMS/Training.Web/Register.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Register.aspx.cs" Inherits="Training.Web.Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 186px">
+    <form id="form1" runat="server">
+    <div style="text-align:center">
+    <asp:Label ID="UserNameRegister" runat="server" Text="用户名："></asp:Label>
+        <asp:TextBox ID="UserNameTextRegister" runat="server" OnTextChanged="UserNameTextRegister_TextChanged"></asp:TextBox>
+        <br />
+        <asp:Label ID="PasswordRegister" runat="server" Text="密码："></asp:Label>
+        <asp:TextBox ID="PasswordTextRegister" runat="server" OnTextChanged="PasswordTextRegister_TextChanged"></asp:TextBox>
+        <br />
+        <asp:Label ID="EmailAddress" runat="server" Text="Email："></asp:Label>
+        <asp:TextBox ID="EmailAddressText" runat="server" OnTextChanged="EmailAddressText_TextChanged"></asp:TextBox>
+        <br />
+        <asp:Label ID="Country" runat="server" Text="Country:"></asp:Label>
+        <asp:DropDownList ID="CountryDropDownList" runat="server">
+        </asp:DropDownList>
+        <br />
+        <asp:Label ID="Region" runat="server" Text="Region:"></asp:Label>
+        <asp:DropDownList ID="RegionDropDownList" runat="server">
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Button ID="RegisterButton" runat="server" OnClick="RegisterButton_Click" Text="提交注册信息" />
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ Training.CMS/Training.Web/Register.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Training.Web
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void RegisterButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void UserNameTextRegister_TextChanged(object sender, EventArgs e)
+        {
+            if (UserNameTextRegister.Text.Trim() == "")
+            {
+                Response.Write("<script>alert('请注意：用户名不能为空！');</script>");
+            }
+            else
+            {
+
+            }
+
+
+
+
+        }
+
+        protected void PasswordTextRegister_TextChanged(object sender, EventArgs e)
+        {
+            if (PasswordTextRegister.Text.Trim() == "")
+            {
+                Response.Write("<script>alert('请注意：密码不能为空！');</script>");
+            }
+            else if (PasswordTextRegister.Text.Trim().Length > 20)
+            {
+                Response.Write("<script>alert('请注意：密码的长度不能超过20！');</script>");
+            }
+            else
+            {
+
+            }
+        }
+
+        protected void EmailAddressText_TextChanged(object sender, EventArgs e)
+        {
+            Regex regemail = new Regex(@"^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$");
+            if (EmailAddressText.Text.Trim() == "")
+            {
+                Response.Write("<script>alert('请注意：Email不能为空！');</script>");
+            }
+            else if (!regemail.IsMatch(EmailAddressText.Text))
+            {
+                Response.Write("请填写正确的Email地址!");
+            }
+            else
+            {
+
+            }
+
+        }
+
+
+
+    }
+}
