d50054d443e7dea5a4f7601a42cbf6b2095c33f9
StevenDHA <sbyqh88888@163.com>
2014-12-10 16:32:36 +0800

    IMovieStore.cs

---
+++ Training.CMS/Training.Data/IMovieStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Training.Domain;
+
+namespace Training.Data
+{
+    public interface IMovieStore
+    {
+        /// <summary>
+        /// add a movie
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
+        int AddMovie(Movie movie);
+        /// <summary>
+        /// update a movie
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
+        int UpdateMovie(Movie movie);
+        /// <summary>
+        /// delete a movie
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns></returns>
+        int DeleteMovie(Movie movie);
+        /// <summary>
+        /// get all movies
+        /// </summary>
+        /// <returns></returns>
+        DataTable GetMovies();
+
+
+    }
+}
