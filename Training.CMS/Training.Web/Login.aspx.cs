129749b9b68981520e8b8810d24901b08f8c5461
mitangtang <1048329163@qq.com>
2014-12-10 16:45:42 +0800

    Login

---
+++ Training.CMS/Training.Web/Login.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Training.Web
+{
+    public partial class Login : System.Web.UI.Page
+    {
+
+       
+    }
+}
